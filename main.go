4b6228681a666e74019c9939d453d77d2839666f
sujonbiswasdev <sujonbiswasdev@gmail.com>
2025-09-25 16:33:27 +0600

    su

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ func main() {
   fmt.Println(30)
   // sujon biswas
 
+
   var student1 string = "John" //type is string
   var student2 = "Jane" //type is inferred
   x := 2 //type is inferred
@@ -14,4 +15,18 @@ func main() {
   fmt.Println(student1)
   fmt.Println(student2)
   fmt.Println(x)
+  // input : Scan,Scanln,Scanf
+  //output : Print,Println,Printf
+
+  var num1,num2 int
+  fmt.Print("enter two number : ")
+  fmt.Scan(&num1,&num2)
+  fmt.Printf("num1 = %v, num2=%v\n",num1,num2)
+
+
+  var fullname string
+  fmt.Print("enter your name")
+  fmt.Scan(&fullname)
+  fmt.Printf("%v is a student\n",fullname)
+
 }
