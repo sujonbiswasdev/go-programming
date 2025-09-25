a8aac0f9c92f9c2921ff28e59061abc02484a377
sujonbiswasdev <sujonbiswasdev@gmail.com>
2025-09-25 16:22:41 +0600

    s

--- main.go
+++ main.go
@@ -3,4 +3,15 @@ import ("fmt")
 
 func main() {
   fmt.Println("Hello World!")
+  fmt.Println("Hello sujon!")
+  fmt.Println(30)
+  // sujon biswas
+
+  var student1 string = "John" //type is string
+  var student2 = "Jane" //type is inferred
+  x := 2 //type is inferred
+
+  fmt.Println(student1)
+  fmt.Println(student2)
+  fmt.Println(x)
 }
