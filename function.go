0be3d803e52ed6f471e6bf11974afad83309e076
sujonbiswasdev <sujonbiswasdev@gmail.com>
2025-09-27 17:08:20 +0600

    funtion

---
+++ function.go
@@ -0,0 +1,12 @@
+package main
+import("fmt")
+
+func myMessage() {
+  fmt.Println("I just got executed!")
+}
+func main() {
+	fmt.Println("funtion")
+	myMessage()
+	
+
+}
--- loop.go
+++ loop.go
@@ -8,7 +8,45 @@ func main() {
     // fmt.Println("bangladesh")
     //  }
 
-	for i:=1 ;i<6; i++ {
-		fmt.Println("Bangladesh")
-	}
+	// for i:=1 ;i<6; i++ {
+	// 	fmt.Println("Bangladesh")
+	// }
+
+	// for i:=0;i<6;i++{
+	// 	if i==3{
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// for i:=0;i<6;i++{
+	// 	if i==3{
+	// 		break
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// print numbers from 1 to 100
+
+	// for x:=2;x<=100;x+=1 {
+	// 	if x%3==0 {
+	// 		fmt.Println("odd number",x)
+	// 	}else{
+	// 		fmt.Println("even number ",x)
+	// 	}
+	// }
+
+	// var num1,num2 int
+	// fmt.Printf("enter first number : ")
+	// fmt.Scan(&num1)
+
+	// fmt.Printf("enter secound number : ")
+	// fmt.Scan(&num2)
+
+	// sum:= 0 
+	// for i:= num1; i < num2; i=i+1 {
+	//      sum=sum + i
+	// }
+	// fmt.Printf("total sum numer %v\n ", sum)
+
 }
