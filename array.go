4c48b51fa9b1a1cee8fb0caae0da04e4a5ff7935
sujonbiswasdev <sujonbiswasdev@gmail.com>
2025-09-27 21:57:25 +0600

    done

--- array.go
+++ array.go
@@ -2,32 +2,32 @@ package main
 import("fmt")
 
 func main(){
-	// fmt.Println("array")
-	// var cars = [4]string{"Volvo", "BMW", "Ford", "Mazda"}
-    // // for i:=0;i<4;i++ {
-	// // 	fmt.Println(cars[i])
-	// // }
-	// i:=true
-
-	// for i==true{
-	// var choice int
-	// fmt.Printf("enter your choice between 0-4 : ")
-	// fmt.Scan(&choice)
-	// fmt.Println(cars[choice])
+	fmt.Println("array")
+	var cars = [4]string{"Volvo", "BMW", "Ford", "Mazda"}
+    // for i:=0;i<4;i++ {
+	// 	fmt.Println(cars[i])
 	// }
+	i:=true
 
-	// var studentName [] string
-	// var studentsName string
-	// var numberofStudent int
-	// fmt.Printf("how many students : ")
-	// fmt.Scan(&numberofStudent)
-	// for i:=0;i<numberofStudent;i++{
-	// 	fmt.Print("enter student name : ")
-	// 	fmt.Scan(&studentsName)
-	// 	studentName = append(studentName,studentsName)
-	// }
-	// fmt.Println(studentName)
-	// fmt.Println(len(studentName))
+	for i==true{
+	var choice int
+	fmt.Printf("enter your choice between 0-4 : ")
+	fmt.Scan(&choice)
+	fmt.Println(cars[choice])
+	}
+
+	var studentName [] string
+	var studentsName string
+	var numberofStudent int
+	fmt.Printf("how many students : ")
+	fmt.Scan(&numberofStudent)
+	for i:=0;i<numberofStudent;i++{
+		fmt.Print("enter student name : ")
+		fmt.Scan(&studentsName)
+		studentName = append(studentName,studentsName)
+	}
+	fmt.Println(studentName)
+	fmt.Println(len(studentName))
 
 	// done array and slice
 
