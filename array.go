762df6668e4917d913326ba9b95ad0536b680f5c
sujonbiswasdev <sujonbiswasdev@gmail.com>
2025-09-27 21:43:20 +0600

    array

---
+++ array.go
@@ -0,0 +1,26 @@
+package main
+import("fmt")
+
+func main(){
+	// fmt.Println("array")
+	// var cars = [4]string{"Volvo", "BMW", "Ford", "Mazda"}
+    // // for i:=0;i<4;i++ {
+	// // 	fmt.Println(cars[i])
+	// // }
+	// i:=true
+
+	// for i==true{
+	// var choice int
+	// fmt.Printf("enter your choice between 0-4 : ")
+	// fmt.Scan(&choice)
+	// fmt.Println(cars[choice])
+	// }
+
+	// var studentName [5] string
+	// for i:=0;i<5;i++{
+	// 	fmt.Print("enter student name : ")
+	// 	fmt.Scan(&studentName[i])
+	// }
+	// fmt.Println(studentName)
+	
+}
